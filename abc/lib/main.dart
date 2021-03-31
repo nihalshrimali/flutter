c539b853668f4bc963cd5742d61cd8d0964ee108
nihalshrimali <81704728+nihalshrimali@users.noreply.github.com>
2021-03-31 23:12:15 +0530

    31-03-2021

--- abc/lib/main.dart
+++ abc/lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            child: Text("hello world"),
+            color: Colors.cyan,
+            width: 100,
+            height: 100,
+          ),
+        ),
+      ),
+    );
+  }
+}
